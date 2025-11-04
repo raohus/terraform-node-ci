b39b8675fd759af515fb0939b3be30f6de25d0b6
raohus <saiprar24@gmail.com>
2025-11-04 08:42:20 +0530

    Update main.tf HTTP  and ICMP

--- main.tf
+++ main.tf
@@ -7,11 +7,31 @@ resource "aws_key_pair" "deployer" {
   public_key = file("${path.module}/deployer_key.pub")
 }
 
-resource "aws_security_group" "ssh_access" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "web_access" {
+  name        = "allow_http_icmp_ssh"
+  description = "Allow HTTP, ICMP, and SSH inbound traffic"
 
+  # HTTP rule
   ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ICMP rule
+  ingress {
+    description = "ICMP (Ping)"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # SSH rule
+  ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -30,10 +50,9 @@ resource "aws_instance" "web" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI for us-east-1
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
-  security_groups = [aws_security_group.ssh_access.name]
+  security_groups = [aws_security_group.web_access.name]
 
   tags = {
     Name = "Terraform-EC2"
   }
 }
-
