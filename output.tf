640f630b23f6ad7098970bbcdc3c7eab15b41b3c
raohus <saiprar24@gmail.com>
2025-11-08 20:21:15 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,15 @@
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
 
-output "ec2_public_ip" {
-  value = aws_instance.web.public_ip
+output "instance_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.web.public_dns
+}
+
+output "environment" {
+  description = "Deployment environment"
+  value       = var.environment
 }
 
